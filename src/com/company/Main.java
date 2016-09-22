fb768f90b737bf8e0cc130498444ed174de60327
Jessica N Qin <jq210@pvhs-lab112-d15.pvsd.org>
2016-09-22 14:20:42 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //sum from three to seventy nine
+        int a = 3;
+        int sum = 0;
+        for (a = 3; a <=79; a++)
+        {
+            //79 - a + 1 =77 This works when I have <=
+            sum = sum + a;
+            System.out.println("Loop iteration: " + (a-2));
+            System.out.println("Value of a: " + a);
+            System.out.println("Value of Sum: " + sum);
+            //break;
+            double b = 3.345;
+        }
+    }
+}
